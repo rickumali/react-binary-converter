784ec603bc31b4d27f3dbd05a813aa8efc6afa08
Rick Umali <rickumali@gmail.com>
2022-11-19 15:47:01 -0500

    Displays Bits under text field
    You see what's going on: enter a number and get the
    binary below. Adjust the bits and get the decimal above.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Bit from './Bit';
 
 function App() {
+  function renderBit(i) {
+    return <Bit />;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         Bit Converter
       </header>
       <input className="Decimal-input" type='text'/>
+      <p/>
+      {renderBit(7)}
+      {renderBit(6)}
+      {renderBit(5)}
+      {renderBit(4)}
+      {renderBit(3)}
+      {renderBit(2)}
+      {renderBit(1)}
+      {renderBit(0)}
     </div>
   );
 }
