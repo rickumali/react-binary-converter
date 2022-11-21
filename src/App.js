477b35e59d1b21064d3c77cb12e1bed9b1b100af
Rick Umali <rickumali@gmail.com>
2022-11-21 09:24:02 -0500

    Displays binary number at last
    Rather than converting the App() to a class component, I
    follow:
    
    https://www.twilio.com/blog/react-choose-functional-components
    
    and use the React.useState() mechanism. This works! And I need to
    think about what it is I just did!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Bit from './Bit';
 
 function App() {
+  const [binaryNumberStr, setBinaryNumberStr] = React.useState("0");
+
   function dec2bin(dec) {
     return (dec >>> 0).toString(2);
   }
@@ -14,6 +17,7 @@ function App() {
   function handleChange(event) {
     console.log(event.target.value);
     console.log(dec2bin(parseInt(event.target.value)).padStart(8, '0'));
+    setBinaryNumberStr(dec2bin(parseInt(event.target.value)).padStart(8, '0'));
   }
 
   return (
@@ -24,7 +28,7 @@ function App() {
       <input className="Decimal-input" type='text'
         onChange={handleChange} />
       <p/>
-      Binary:
+      Binary: {binaryNumberStr}
       <p/>
       {renderBit(false)}
       {renderBit(false)}
