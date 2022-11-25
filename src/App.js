3d3fc23456dbe74ec0c0e7d7915415773282ecc3
Rick Umali <rickumali@gmail.com>
2022-11-24 22:05:14 -0500

    Attempts to "lift the state"
    Adds number to the state
    Removes constructor from Bit (input type=checkbox)
    Modifies Bit so that user can interact with the checkbox
    Creates an input handler for the Bit
    Hardcodes values, until I can figure out how to deal with String copying

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Bit from './Bit';
 
 function App() {
   const [someObject, setSomeObject] = React.useState({
+    number: 0,
     displayString: "00000000",
     error: ""
   });
@@ -13,14 +14,37 @@ function App() {
     return (dec >>> 0).toString(2);
   }
 
-  function renderBit(one_or_zero) {
+  function handleBitChange(b) {
+    let newNumber = someObject.number;
+    let newDisplayString = someObject.displayString.repeat(1);
+    let one_or_zero = someObject.displayString[b]
+    console.log("Help me Obi " + b + " " + one_or_zero);
+    if (one_or_zero == "1") {
+      // TODO: newDisplayString[b] = 0;
+    } else if (one_or_zero == "0") {
+      // TODO: newDisplayString[b] = 1;
+    }
+    setSomeObject({
+      number: 11, // TODO: FIX. Hardcoded
+      displayString: "00001011", // TODO: FIX. Hardcoded
+      error: ""
+    });
+  }
+
+  function renderBit(bit) {
     let checked = false;
+    let one_or_zero = someObject.displayString[bit]
     if (one_or_zero == "1") {
       checked = true;
     } else if (one_or_zero == "0") {
       checked = false;
     }
-    return <Bit seton={checked}/>;
+    return (
+      <Bit
+        seton={checked}
+        onChange={() => handleBitChange(bit)}
+      />
+    );
   }
 
   function handleChange(event) {
@@ -29,6 +53,7 @@ function App() {
       number = 0;
       event.target.value = 0;
       setSomeObject({
+        number: number,
         displayString: "00000000",
         error: "Number must be between 0 and 255"
       });
@@ -37,7 +62,9 @@ function App() {
     console.log(dec2bin(number).padStart(8, '0'));
     let binString = dec2bin(number).padStart(8, '0');
     setSomeObject({
-      displayString: binString
+      number: number,
+      displayString: binString,
+      error: ""
     });
   }
 
@@ -46,19 +73,19 @@ function App() {
       <header className="App-header">
         Bit Converter
       </header>
-      <input className="Decimal-input" type='text'
+      <input className="Decimal-input" type='text' value={someObject.number}
         onChange={handleChange} />
       <p/>
       Binary: {someObject.displayString}
       <p/>
-      {renderBit(someObject.displayString[0])}
-      {renderBit(someObject.displayString[1])}
-      {renderBit(someObject.displayString[2])}
-      {renderBit(someObject.displayString[3])}
-      {renderBit(someObject.displayString[4])}
-      {renderBit(someObject.displayString[5])}
-      {renderBit(someObject.displayString[6])}
-      {renderBit(someObject.displayString[7])}
+      {renderBit(0)}
+      {renderBit(1)}
+      {renderBit(2)}
+      {renderBit(3)}
+      {renderBit(4)}
+      {renderBit(5)}
+      {renderBit(6)}
+      {renderBit(7)}
       <p/>
       {someObject.error}
     </div>
--- src/Bit.js
+++ src/Bit.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 class Bit extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    return <input className="Bit" type='checkbox' checked={this.props.seton}/>;
+    return (
+      <input
+        className="Bit"
+        type='checkbox'
+        readOnly={false}
+        checked={this.props.seton}
+        onChange={() => this.props.onChange()}
+      />
+    );
   }
 }
 
